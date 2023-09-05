0185ee5dabc5ef5dc57888fc299a61ae1d871eac
yao tang <wiltchamberian@163.com>
2023-09-05 13:18:22 +0800

    level works

--- level.py
+++ level.py
@@ -1,6 +1,12 @@
 import os
+import re
 
 folder_path = "./userdata"
+
+def custom_sort(a):
+    parts = re.split(r'[\\/\\-]', a)
+    int_parts = [int(part) for part in parts] 
+    return tuple(int_parts)
 
 class File:
   def __init__(self):
@@ -14,9 +20,33 @@ def extract_r(folder_path ,file_list):
                 myfile = File()
                 myfile.filepath = os.path.join(folder, file)
                 file_list.append(myfile)
-
+        if(len(subfolders)>0):
+            if subfolders[0].isdigit(): #for numbers
+                subfolders.sort(key = lambda x : int(x))
+            else:
+                #sorted(subfolders, cmp = custom_sort )
+                #Density.sort(key = getSecond, reverse = True);
+                subfolders.sort(key = custom_sort )
         for folder in subfolders:
             extract_r(folder_path +'/' + folder, file_list)
+        break
+
+# def extract_r(folder_path ,file_list):
+#     items = os.listdir(folder_path)
+#     dirs = []
+#     files = []
+#     for item in items:
+#         item_path = os.path.join(folder_path, item)
+#         if os.path.isdir(item_path):
+#             dirs.append(item_path)
+            
+#         else:
+#             myfile = File()
+#             myfile.filepath = item_path
+#             file_list.append(myfile)
+
+#         extract_r(item_path, file_list)
+        
 
 def extract_files_from_folder(folder_path):
     file_list = []
@@ -24,12 +54,18 @@ def extract_files_from_folder(folder_path):
     return file_list
     
 def make_time_stamp(file_list):
-    for file in file_list:
-        pass
-        
-    
+    pass
+      
+  
 
 if __name__ == "__main__":
     file_list = extract_files_from_folder(folder_path)
+    
+    file_path = "output.txt"
+    with open(file_path, "w") as file:
+        # 遍历字符串数组并将每个字符串写入文件
+        for string in file_list:
+            file.write(string.filepath + "\n")
+
     print("done")
     
