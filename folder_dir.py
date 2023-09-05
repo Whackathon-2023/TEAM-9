bd881d813ec3fd6f59e37fb43adfd64ea5935b6d
0scarTheCoder <127009348+0scarTheCoder@users.noreply.github.com>
2023-09-05 13:26:03 +0800

    Create folder_dir.py

---
+++ folder_dir.py
@@ -0,0 +1,41 @@
+import os
+import re
+from datetime import datetime
+
+folder_path = "./Problem06_Still_No_Longer/TEAM-9/userdata"
+
+class File:
+    def __init__(self):
+        self.filepath= ""
+        self.timestamp = None
+
+def extract_r(folder_path, file_list):
+    for folder, subfolders, files in os.walk(folder_path):
+        if not subfolders:
+            for file in files:
+                myfile = File()
+                myfile.filepath = os.path.join(folder, file)
+                myfile.timestamp = extract_timestamp(myfile.filepath)
+                file_list.append(myfile)
+
+def extract_timestamp(filepath):
+    # Extract timestamp from the file path using regular expressions
+    match = re.search(r'(\d{4})/(\d+)/(\d+)/(\d+)-(\d+)', filepath)
+    if match:
+        year, month, day, hour, minutes = map(int, match.groups())
+        return datetime(year, month, day, hour, minutes)
+    return None
+
+def extract_files_from_folder(folder_path):
+    file_list = []
+    extract_r(folder_path, file_list)
+    return sorted(file_list, key=lambda x: x.timestamp)
+
+if __name__ == "__main__":
+    file_list = extract_files_from_folder(folder_path)
+    
+    # Print the sorted list of file paths
+    for file in file_list:
+        print(file.filepath)
+
+    print("done")
